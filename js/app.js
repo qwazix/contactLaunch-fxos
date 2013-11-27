1f9ae11902417a3dcee2ce4952795dce281c7412
Michael Demetriou <qwazix@gmail.com>
2013-11-27 16:30:55 +0200

    fixed blob post (base64)

--- js/app.js
+++ js/app.js
@@ -46,13 +46,21 @@ var app = function(){
 					person.id = cursor.result.id;
 					person.name = cursor.result.name || '';
 					person.tel = cursor.result.tel ?  cursor.result.tel[0].value : '';
-                    if (cursor.result.photo!=null && cursor.result.photo[0]!=null)
+                    if (cursor.result.photo!=null && cursor.result.photo[0]!=null){
                         person.photo = window.URL.createObjectURL(cursor.result.photo[0]);
-                    else 
+                        var reader = new FileReader();
+                        reader.readAsDataURL(cursor.result.photo[0]);//Convert the blob from clipboard to base64
+                        reader.onload = function(event){
+                            person.imageData = event.target.result;
+//                            console.log(person.imageData);
+                            $$('#c'+person.id).attr("data-imgdata",person.imageData);
+//                            var base64Content = imageData.substring(imageData.indexOf(',') + 1, imageData.length);
+                        }
+                    }else {
                         person.photo = "images/avatar.png";
-                    
-					
-			  	tmpl += '<li>';
+                        person.imageData = "";
+                    }
+			  	tmpl += '<li id="c' + person.id + '">';
                 tmpl += '	<img src="' + person.photo + '">';
 			  	tmpl += '	<a href="#">';
 			  	tmpl += '	<p class=personname>' + person.name + '</p>';
